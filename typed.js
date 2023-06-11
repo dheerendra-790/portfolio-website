86448e809fc56fbc53ae99cedefa40190a5a0de1
dheerendra-790 <136226157+dheerendra-790@users.noreply.github.com>
2023-06-11 14:04:02 +0530

    typed.js

--- typed.js
+++ typed.js
@@ -1,10 +1,10 @@
-var typed = new Typed(".multiple-text",{
-    strings:["front-end developer", "Ui/UX designer",],
-    typeSpeed: 80,
-    backSpeed: 40,
-    backDelay: 100,
-    loop:true
-})
+// var typed = new Typed(".multiple-text",{
+//     strings:["front-end developer", "Ui/UX designer",],
+//     typeSpeed: 80,
+//     backSpeed: 40,
+//     backDelay: 100,
+//     loop:true
+// })
 // var typed = new Typed(".multiple-text", {
 //     strings: ["frontend developer", "Ui/UX designer","Blogger", "Content-writer"],
 //     typeSpeed: 100,
@@ -13,11 +13,11 @@ var typed = new Typed(".multiple-text",{
 //     loop: true
 // });
 
-// function dropdown(){
-//     var toggleMenu = document.querySelector(".menu");
-//     toggleMenu.classList.toggle('activate')
+function dropdown(){
+    var toggleMenu = document.querySelector(".menu");
+    toggleMenu.classList.toggle('activate')
 
-// }
+}
 
 // var typed =new Typed('.multiple-text', {
 //     strings: ['Front-end developer', 'UI/UX designer', 'Bootstrap framewords'],
