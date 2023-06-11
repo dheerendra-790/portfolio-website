087d3b9cd1c18ff19fb93b914bfb2916ec6c4700
dheerendra-790 <136226157+dheerendra-790@users.noreply.github.com>
2023-06-11 13:44:38 +0530

    js

---
+++ typed.js
@@ -0,0 +1,25 @@
+var typed = new Typed(".multiple-text",{
+    strings:["front-end developer", "Ui/UX designer",],
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 100,
+    loop:true
+})
+// var typed = new Typed(".multiple-text", {
+//     strings: ["frontend developer", "Ui/UX designer","Blogger", "Content-writer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+
+// function dropdown(){
+//     var toggleMenu = document.querySelector(".menu");
+//     toggleMenu.classList.toggle('activate')
+
+// }
+
+// var typed =new Typed('.multiple-text', {
+//     strings: ['Front-end developer', 'UI/UX designer', 'Bootstrap framewords'],
+//     typeSpeed: 50,
+//   });
